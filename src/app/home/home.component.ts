930cd9df848cd57c57b03bc247526a2187bf91fb
Jerry Hirsch <codehustle@GovDev.local>
2020-04-23 12:01:33 -0400

    Tests added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from '../services/users/users.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  users;
+  constructor(private usersService: UsersService) { }
+  ngOnInit() {
+    this.usersService.all().subscribe(res => {
+      this.users = res;
+    });
   }
-
 }
